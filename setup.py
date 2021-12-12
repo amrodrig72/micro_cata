55f5f23db4affb2fb7e6379074f134d90e06c8c5
Andres Rodriguez <amrodrig@caltech.edu>
2021-12-12 01:02:41 -0800

    why

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     author='Andres Rodriguez and Adiel Perez',
     author_email='amrodrig@caltech.edu',
     description='Package that will help us analyze microtuble catastrophe data.',
-    long_description=README.md,
+    long_description=long_description,
     long_description_content_type='markdown',
     packages=setuptools.find_packages(),
     install_requires=["numpy","pandas","bokeh>=1.4.0"],
